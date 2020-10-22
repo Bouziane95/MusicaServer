c3c47b571cfab1552ee20909b196d5ff24582523
Bouziane <bey.bouziane@live.fr>
2020-10-22 15:01:05 +0200

    modif deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66241c1e7b3ea50543e46f8af3b82eac.js"
+  "/precache-manifest.8f67781890e2be3fe137739e69c3077f.js"
 );
 
 self.addEventListener('message', (event) => {
